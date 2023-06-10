f329e658df2f8fa4722b9e237ca385b0f37d15c9
eliasjp <illumine1998@gmail.com>
2023-06-10 14:53:33 -0300

    fix text return

--- frontend/src/components/typography/text.components.tsx
+++ frontend/src/components/typography/text.components.tsx
@@ -8,9 +8,9 @@ interface TextSchema {
 export function Text ({type, weight, extra_classes="", children}: TextSchema){
     if (extra_classes) extra_classes += " " + extra_classes
 
-    if (type == "b1" && weight == 600){ return ( <h1 className={`text-[16px] leading-[28px] font-semibold font-inter${extra_classes}`}>{children}</h1> ) }
-    if (type == "b1" && weight == 400){ return ( <h1 className={`text-[16px] leading-[28px] font-normal font-inter${extra_classes}`}>{children}</h1> ) }
+    if (type == "b1" && weight == 600){ return ( <p className={`text-[16px] leading-[28px] font-semibold font-inter${extra_classes}`}>{children}</p> ) }
+    if (type == "b1" && weight == 400){ return ( <p className={`text-[16px] leading-[28px] font-normal font-inter${extra_classes}`}>{children}</p> ) }
 
-    if (type == "b2" && weight == 500){ return ( <h1 className={`text-[14px] leading-[24px] font-medium font-inter${extra_classes}`}>{children}</h1> ) }
-    if (type == "b2" && weight == 400){ return ( <h1 className={`text-[14px] leading-[24px] font-normal font-inter${extra_classes}`}>{children}</h1> ) }
+    if (type == "b2" && weight == 500){ return ( <p className={`text-[14px] leading-[24px] font-medium font-inter${extra_classes}`}>{children}</p> ) }
+    if (type == "b2" && weight == 400){ return ( <p className={`text-[14px] leading-[24px] font-normal font-inter${extra_classes}`}>{children}</p> ) }
 }
