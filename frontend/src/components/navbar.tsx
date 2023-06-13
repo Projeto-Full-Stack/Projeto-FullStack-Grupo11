15bbb81ce039bd59c0797035dae603be7ad5c7ff
Edmilson <krakadianbill@gmail.com>
2023-06-13 16:15:49 -0300

    fix: navbar CSS]

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -15,11 +15,9 @@ const NavBar = () => {
 
     return (
         <>
-            <header className='flex justify-between items-center px-[16px] py-[26px] bg-grey-grey_10 border-'>
-                <div>
-                    <Heading type="h3" weight={600} extra_classes="bg-clip-text text-transparent bg-gradient-to-tr from-grey-grey_0 to-brand-brand_1">Motors shops</Heading>
-                </div>
-                <div className="lg:border-grey-grey_6 lg:border-l-[2px] lg:items-center lg:px-5 lg:min-h-full hidden lg:block">
+            <header className='flex justify-between items-center px-[16px] h-[80px] bg-grey-grey_10 border-'>
+                <Heading type="h3" weight={600} extra_classes="bg-clip-text text-transparent bg-gradient-to-tr from-grey-grey_0 to-brand-brand_1">Motors shops</Heading>
+                <div className="lg:border-grey-grey_6 items-center lg:border-l-[2px] h-full py-[20px] lg:items-center lg:px-5 lg:min-h-full hidden lg:block">
                     <Button type={"bg-light"} children={"Fazer login"} extra_classes="mx-2"/>
                     <Button type={"bg-light"} children={"Cadastrar"}/>
                 </div>
