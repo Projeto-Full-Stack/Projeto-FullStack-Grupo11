f5b6bb0f6015cf92ce8d5f67b9b49e507f62e21f
Mayza <mayza.santos@outlook.com.br>
2023-06-14 13:04:53 -0300

    feat: create modules users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnnouncementsModule } from './modules/announcements/announcements.module';
 import { ImagesModule } from './modules/images/images.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AnnouncementsModule, ImagesModule],
+  imports: [AnnouncementsModule, ImagesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
