ef93c1bbda3bba38ca87093daae064ad94785465
Edmilson <krakadianbill@gmail.com>
2023-06-16 12:59:27 -0300

    refactoring: CSS classses

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -15,11 +15,11 @@ const NavBar = () => {
 
     return (
         <>
-            <header className="flex justify-between items-center px-[16px] h-[80px] border-b-2 border-grey-grey_6 bg-grey-grey_10 3">
-                <Heading type="h3" weight={600} extra_classes="bg-clip-text text-transparent bg-gradient-to-tr from-grey-grey_0 to-brand-brand_1">Motors shops</Heading>
-                <div className="lg:border-grey-grey_6 items-center lg:border-l-[2px] h-full py-[20px] lg:items-center lg:px-5 lg:min-h-full hidden lg:block">
-                    <Button type={"bg-none"} children={"Fazer login"} extra_classes="mx-2 "/>
-                    <Button type={"b-black"} children={"Cadastrar"}/>
+            <header className="flex justify-between items-center px-[16px] h-[80px] border-b-2 border-grey-6 bg-grey-10">
+                <Heading type="h3" weight={600} extra_classes="bg-clip-text text-transparent bg-gradient-to-tr from-grey-0 to-brand-1">Motors shops</Heading>
+                <div className="lg:border-grey-6 items-center lg:border-l-[2px] h-full py-[20px] lg:items-center lg:px-5 lg:min-h-full hidden lg:block">
+                    <Button type={"bg-none"} extra_classes="mx-2 ">Fazer Login</Button>
+                    <Button type={"b-black"}>Registrar</Button>
                 </div>
                 <button className="lg:hidden" onClick={showNavigation}>{textButton()}</button>
             </header>
