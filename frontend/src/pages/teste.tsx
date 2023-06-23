b2d7bf1131670778e679b3c2e6e282aac4c3605f
eliasjp <illumine1998@gmail.com>
2023-06-23 09:48:07 -0300

    feat: create disable option for inputs

---
+++ frontend/src/pages/teste.tsx
@@ -0,0 +1,11 @@
+import Input from "@/components/Inputs/input"
+
+
+export default function Teste (){
+
+    return (
+        <>
+            <Input input_name="teste" input_type="text" label="teste" rest={`disable`}>Teste</Input>
+        </>
+    )
+}
