0cdc614a6c3b59e737051cd47f0ebb79cd6ab411
Edmilson <krakadianbill@gmail.com>
2023-06-29 15:27:04 -0300

    feat: added the final logic to comments module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { ImagesModule } from './modules/images/images.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AddressModule } from './modules/address/address.module';
+import { CommentsModule } from './modules/comments/comments.module';
 
 @Module({
-  imports: [AnnouncementsModule, ImagesModule, UsersModule, AuthModule, AddressModule],
+  imports: [AnnouncementsModule, ImagesModule, UsersModule, AuthModule, AddressModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
