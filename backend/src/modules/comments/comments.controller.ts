a6dd19356669ce5c680b90df710e8e107ff08b5f
eliasjp <illumine1998@gmail.com>
2023-06-30 13:13:35 -0300

    feat: create comments controllers  documentation

--- backend/src/modules/comments/comments.controller.ts
+++ backend/src/modules/comments/comments.controller.ts
@@ -15,6 +15,7 @@ import { CreateCommentDto } from "./dto/create-comment.dto";
 import { JwtAuthGuard } from "../auth/jawt.auth.guard";
 import { CommentService } from "./comments.service";
 import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiResponse, ApiCreatedResponse } from '@nestjs/swagger/dist/decorators';
 
 @ApiTags('Comments')
 @Controller('comments')
@@ -22,6 +23,15 @@ export class CommentsController{
     constructor(private readonly commentsService: CommentService){}
 
     @Post(':announcementId')
+    @ApiCreatedResponse({ description: "Cria um comentário para um anúncio, recebe o ID do anúncio pelo parametro e o ID do usuário através do token", schema: {
+        example: {
+            "id": "d25aa8fe-b6cc-4b2e-ba0a-c45975fb3b88",
+            "comment": "teste",
+            "createdAt": "2023-06-30T12:35:56.595Z",
+            "authorId": "84ca3ccf-1639-4cc4-9aa1-1c583537205d",
+            "announcementId": "398eb6a4-62d6-41fe-a92c-c4f7a296d2a4"
+        }
+    }})
     @UseGuards(JwtAuthGuard)
     create(@Param('announcementId') announcementId: string, @Body() CreateCommentDto: CreateCommentDto, @Request() req: any){
         return this.commentsService.create(CreateCommentDto, announcementId, req.user.id)
@@ -33,11 +43,49 @@ export class CommentsController{
     }
 
     @Get(':announcementId')
+    @ApiOkResponse({ description: "Recebe todos os comentários do anúncio passado pelo ID", schema: {
+        example: [
+            {
+                "id": "45fcc392-1b18-4a40-bfeb-9357e5c1ec04",
+                "comment": "teste",
+                "createdAt": "2023-06-30T12:33:06.699Z",
+                "authorId": "84ca3ccf-1639-4cc4-9aa1-1c583537205d",
+                "announcementId": "757ad206-b887-4fac-9f23-a7319001f764",
+                "author": {
+                    "id": "84ca3ccf-1639-4cc4-9aa1-1c583537205d",
+                    "name": "teste",
+                    "description": "1234567"
+                }
+            },
+            {
+                "id": "bbd8bfdf-967f-4b55-99cc-b8f2834cadc9",
+                "comment": "teste",
+                "createdAt": "2023-06-30T12:34:54.300Z",
+                "authorId": "84ca3ccf-1639-4cc4-9aa1-1c583537205d",
+                "announcementId": "757ad206-b887-4fac-9f23-a7319001f764",
+                "author": {
+                    "id": "84ca3ccf-1639-4cc4-9aa1-1c583537205d",
+                    "name": "teste",
+                    "description": "1234567"
+                }
+            }
+        ]
+    }})
     findAll(@Param('announcementId') annoucementId: string){
         return this.commentsService.findAll(annoucementId)
     }
 
     @Patch(':commentId')
+    @ApiOkResponse({ description: "Recebe o novo comentário e atualiza", schema: {
+        example: {
+            "id": "d25aa8fe-b6cc-4b2e-ba0a-c45975fb3b88",
+            "comment": "new comment",
+            "createdAt": "2023-06-30T12:35:56.595Z",
+            "authorId": "84ca3ccf-1639-4cc4-9aa1-1c583537205d",
+            "announcementId": "398eb6a4-62d6-41fe-a92c-c4f7a296d2a4"
+        }
+        
+    }})
     @UseGuards(JwtAuthGuard)
     async update(@Param('commentId') commentId: string, @Body() updateCommentDto: UpdateCommentDto, @Request() req:any){
         const findComment = await this.commentsService.findOne(commentId);
@@ -47,6 +95,8 @@ export class CommentsController{
     }
 
     @Delete(':commentId')
+    @ApiResponse({ status: 204, description: "Deleta o comentário passado pelo parametro" })
+    @ApiResponse({ status: 404})
     @UseGuards(JwtAuthGuard)
     async remove(@Param('commentId') commentId: string, @Request() req:any){
         const findComment = await this.commentsService.findOne(commentId);
